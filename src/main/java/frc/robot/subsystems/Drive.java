70c6c2f83ce8ce842ecac73d93204efd7462d854
Otto <grentomato@gmail.com>
2024-09-25 23:36:39 -0400

    Holy, I must have had low thoughts per minute

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -1,11 +1,9 @@
 package frc.robot.subsystems;
 
 import java.io.File;
-import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import swervelib.parser.SwerveParser;
 import swervelib.SwerveDrive;
@@ -14,14 +12,16 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
 
 public class Drive extends SubsystemBase {
-    double maximumSpeed = Units.feetToMeters(12);
-    File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(), "swerve");
-    public SwerveDrive swerveDrive;
-    public XboxController controller;
+    private double maximumSpeed;
+    private File swerveJsonDirectory;
+    private SwerveDrive swerveDrive;
+    private XboxController controller;
 
     public Drive() {
         controller = new XboxController(1);
+        maximumSpeed = Units.feetToMeters(12);
         try {
+            swerveJsonDirectory = new File(Filesystem.getDeployDirectory(), "swerve");
             swerveDrive = new SwerveParser(swerveJsonDirectory).createSwerveDrive(maximumSpeed);
         } catch (Exception e) {
             System.out.println(e);
